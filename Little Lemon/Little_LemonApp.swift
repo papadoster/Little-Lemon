4fbb7c11e075e214e84de9eeae182acc187d20d5
Aleksandr Karpov <papadoster13@gmail.com>
2023-08-31 14:49:20 +0200

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Александр Карпов on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
